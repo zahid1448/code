c05e9c373094ad7b0e957e90c14b6d0acc915f8f
mohd zahid anwar <zahidanwar1995@gmail.com>
2020-07-05 20:19:18 +0530

    Initial Commit

---
+++ src/org/btm/App/Runner.java
@@ -0,0 +1,19 @@
+package org.btm.App;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Runner {
+
+	public static void main(String[] args) {
+
+		System.out.println("creating container");
+		AbstractApplicationContext container = new ClassPathXmlApplicationContext("spring.xml");
+	
+		Human aman = (Human) container.getBean("aman");
+		System.out.println(aman);
+		
+		container.close();
+
+	}
+
+}
